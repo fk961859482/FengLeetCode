d1418db432cd1505f30094754fa815c3e06876c7
fk961859482 <961859482@qq.com>
2021-11-18 01:52:06 +0800

    easy

---
+++ src/main/java/com/fengkang/leetcode/DFS/FlattenBinaryTreetoLinkedList_114.java
@@ -0,0 +1,28 @@
+package com.fengkang.leetcode.DFS;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlattenBinaryTreetoLinkedList_114 {
+	
+	public void flatten(TreeNode root) {
+		if(root == null) {
+			return;
+		}
+		TreeNode ans = new TreeNode(root.val);
+		preOrder(ans, root);
+		return ans;
+	}
+	
+	void preOrder(TreeNode ans, TreeNode root) {
+		List<Integer> a= new ArrayList<>();
+		a.get()
+		if(root == null) {
+			return;
+		}
+		ans.right = root.val;
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+}
---
+++ src/main/java/com/fengkang/leetcode/DFS/NumberOfIslands_200.java
@@ -0,0 +1,9 @@
+package com.fengkang.leetcode.DFS;
+
+public class NumberOfIslands_200 {
+	
+	// 找连通块，即连续的"1"组成的块；dfs+回溯
+	public int numIslands(char[][] grid) {
+	
+	}
+}
