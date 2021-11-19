fc15dbda8ce51ce42f03f5a1219f4a6764f10eb0
fk961859482 <961859482@qq.com>
2021-11-19 23:29:46 +0800

    easy

--- src/main/java/com/fengkang/leetcode/easy/InvertBinaryTree_226.java
+++ src/main/java/com/fengkang/leetcode/easy/InvertBinaryTree_226.java
@@ -1,22 +1,22 @@
-package com.fengkang.leetcode.easy;
-
-import javax.swing.tree.TreeNode;
-
-public class InvertBinaryTree_226 {
-	
-	public TreeNode invertTree(TreeNode root) {
-		bfs(root);
-		return root;
-	}
-	
-	public void bfs(TreeNode root) {
-		if(root == null) {
-			return ;
-		}
-		TreeNode leftt = root.left;
-		root.left = root.right;
-		root.right = leftt;
-		invertTree(root.left);
-		invertTree(root.right);
-	}
-}
+//package com.fengkang.leetcode.easy;
+//
+//import javax.swing.tree.TreeNode;
+//
+//public class InvertBinaryTree_226 {
+//
+//	public TreeNode invertTree(TreeNode root) {
+//		bfs(root);
+//		return root;
+//	}
+//
+//	public void bfs(TreeNode root) {
+//		if(root == null) {
+//			return ;
+//		}
+//		TreeNode leftt = root.left;
+//		root.left = root.right;
+//		root.right = leftt;
+//		invertTree(root.left);
+//		invertTree(root.right);
+//	}
+//}
--- src/main/java/com/fengkang/leetcode/easy/PalindromeLinkedList_234.java
+++ src/main/java/com/fengkang/leetcode/easy/PalindromeLinkedList_234.java
@@ -1,29 +1,28 @@
-package com.fengkang.leetcode.easy;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
-public class PalindromeLinkedList {
-	
-	/**
-	 * 思路1：快慢指针
-	 * 思路2: 前半部分入栈，与后半部分比较
-	 * 思路3：bf做法
-	 * @param head
-	 * @return
-	 */
-	public boolean isPalindrome(ListNode head) {
-		List<Integer> values = new ArrayList<>();
-		while (head != null) {
-			values.add(head.val);
-			head = head.next;
-		}
-		for(int i = 0 ,j = values.size() - 1 ;i < j;i++, j--) {
-			if(!values.get(i).equals(values.get(j))) {
-				return false;
-			}
-		}
-		return true;
-	}
-}
+//package com.fengkang.leetcode.easy;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class PalindromeLinkedList_234 {
+//
+//	/**
+//	 * 思路1：快慢指针
+//	 * 思路2: 前半部分入栈，与后半部分比较
+//	 * 思路3：bf做法
+//	 * @param head
+//	 * @return
+//	 */
+//	public boolean isPalindrome(ListNode head) {
+//		List<Integer> values = new ArrayList<>();
+//		while (head != null) {
+//			values.add(head.val);
+//			head = head.next;
+//		}
+//		for(int i = 0 ,j = values.size() - 1 ;i < j;i++, j--) {
+//			if(!values.get(i).equals(values.get(j))) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//}
